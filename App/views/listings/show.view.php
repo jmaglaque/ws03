e7b19979af82604cf970c05b9f8729778f219443
jmaglaque <jayr.maglaque09153@gmail.com>
2026-05-11 23:00:48 +0800

    updated na uli sa lesson 2 na video ni ser

--- App/views/listings/show.view.php
+++ App/views/listings/show.view.php
@@ -19,21 +19,19 @@
             </div>
         </div>
         <div class="p-4">
-            <h2 class="text-xl font-semibold">Software Engineer</h2>
+            <h2 class="text-xl font-semibold"><?= $listing->title ?? 'Job Title' ?></h2>
             <p class="text-gray-700 text-lg mt-2">
-                We are seeking a skilled software engineer to develop high-quality
-                software solutions.
+                <?= $listing->description ?? 'No description available.' ?>
             </p>
             <ul class="my-4 bg-gray-100 p-4">
-                <li class="mb-2"><strong>Salary:</strong> $80,000</li>
+                <li class="mb-2"><strong>Salary:</strong> <?= formatSalary($listing->salary ?? 'No salary available.') ?></li>
                 <li class="mb-2">
-                    <strong>Location:</strong> New York
+                    <strong>Location:</strong> <?= $listing->city ?? 'No City available' ?>,<?= $listing->state ?? 'No State available' ?>
                     <span
                         class="text-xs bg-blue-500 text-white rounded-full px-2 py-1 ml-2">Local</span>
                 </li>
                 <li class="mb-2">
-                    <strong>Tags:</strong> <span>Development</span>,
-                    <span>Coding</span>
+                    <strong>Tags:</strong> <?= $listing->tags ?? 'No tags available' ?>
                 </li>
             </ul>
         </div>
@@ -47,18 +45,17 @@
             Job Requirements
         </h3>
         <p>
-            Bachelors degree in Computer Science or related field, 3+ years of
-            software development experience
+            <?= $listing->requirements ?? 'No Job requirements available.' ?>
         </p>
         <h3 class="text-lg font-semibold mt-4 mb-2 text-blue-500">Benefits</h3>
-        <p>Healthcare, 401(k) matching, flexible work hours</p>
+        <p><?= $listing->benefits ?? 'No benefits available.' ?></p>
     </div>
     <p class="my-5">
         Put "Job Application" as the subject of your email and attach your
         resume.
     </p>
     <a
-        href="manager@company.com"
+        href="mailto:<?= $listing->email ?? 'No email available.' ?>"
         class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium cursor-pointer text-indigo-700 bg-indigo-100 hover:bg-indigo-200">
         Apply Now
     </a>
