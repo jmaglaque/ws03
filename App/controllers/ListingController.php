ab82c4ddcd2e28e00f46fdaf8dcb59d8a5c97504
jmaglaque <jayr.maglaque09153@gmail.com>
2026-05-15 12:53:22 +0800

    crud part 1

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use Framework\Database;
+use Framework\Validation;
 
 class ListingController
 {
@@ -11,17 +12,21 @@ class ListingController
     public function __construct()
     {
         $config = require basePath('config/db.php');
+
         $this->db = new Database($config);
     }
 
     public function index()
     {
+
         $listings = $this->db->query('SELECT * FROM listings')->fetchAll();
 
-        loadView('listings/index', [
-            'listings' => $listings
-        ]);
+
+
+
+        loadView('listings/index', ['listings' => $listings]);
     }
+
     public function create()
     {
         loadView('listings/create');
@@ -36,7 +41,6 @@ class ListingController
 
         $listing = $this->db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
 
-        //Check if listing exist
         if (!$listing) {
             ErrorController::notFound('Listing not found');
             return;
@@ -45,5 +49,55 @@ class ListingController
         loadView('listings/show', [
             'listing' => $listing
         ]);
+    }
+    /**
+     * Store data in database
+     * 
+     * @return void
+     */
+
+    public function store()
+    {
+        $allowedFields = [
+            'title',
+            'description',
+            'salary',
+            'tags',
+            'company',
+            'address',
+            'city',
+            'state',
+            'phone',
+            'email',
+            'requirements',
+            'benefits'
+        ];
+
+        $newListingData = array_intersect_key($_POST, array_flip($allowedFields));
+
+        $newListingData['user_id'] = 1;
+
+        $newListingData = array_map('sanitize', $newListingData);
+
+        $requiredFields = ['title', 'description', 'email', 'city', 'state'];
+
+        $errors = [];
+
+        foreach ($requiredFields as $field) {
+            if (empty($newListingData[$field]) || !Validation::string($newListingData[$field])) {
+                $errors[$field] = ucfirst($field) . ' is required';
+            }
+        }
+
+        if (!empty($errors)) {
+            //Reload view with errors
+            loadView('listings/create', [
+                'errors' => $errors,
+                'listing' => $newListingData
+            ]);
+        } else {
+            //Submit data
+            echo "Success";
+        }
     }
 }
