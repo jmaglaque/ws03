fad0d48edd35c44646adca6a65dfe79634583ff9
jmaglaque <jayr.maglaque09153@gmail.com>
2026-05-16 15:16:37 +0800

    crud 3

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use Framework\Database;
 use Framework\Validation;
+use VARIANT;
 
 class ListingController
 {
@@ -152,5 +153,89 @@ class ListingController
         $_SESSION['success_message'] = 'Listing delete successfully';
 
         redirect('/listings');
+    }
+
+    public function edit($params)
+    {
+        $id = $params['id'] ?? '';
+        $params = [
+            'id' => $id
+        ];
+
+        $listing = $this->db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
+
+        if (!$listing) {
+            ErrorController::notFound('Listing not found');
+            return;
+        }
+
+        loadView('listings/edit', [
+            'listing' => $listing
+        ]);
+    }
+
+    /**
+     * Update listing
+     * 
+     * @param array $params
+     * @return variant
+     */
+    public function update($params)
+    {
+        $id = $params['id'] ?? '';
+        $params = [
+            'id' => $id
+        ];
+
+        $listing = $this->db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
+
+        if (!$listing) {
+            ErrorController::notFound('Listing not found');
+            return;
+        }
+
+        $allowedFields = [
+            'title',
+            'description',
+            'salary',
+            'tags',
+            'company',
+            'address',
+            'city',
+            'state',
+            'phone',
+            'email',
+            'requirements',
+            'benefits'
+        ];
+
+        $updatedValues = [];
+
+        $updatedValues = array_intersect_key($_POST, array_flip($allowedFields));
+
+        $updatedValues = array_map('sanitize', $updatedValues);
+
+        $requiredFields = ['title', 'description', 'salary', 'email', 'city', 'state'];
+
+        $errors = [];
+
+        foreach ($requiredFields as $field) {
+            if (empty($updatedValues[$field]) || !Validation::string($updatedValues[$field])) {
+                $errors[$field] = ucfirst($field) . 'is required';
+            }
+        }
+        if (!empty($errors)) {
+            \loadView('listings/edit', [
+                'listing' => $listing,
+                'errors' => $errors
+            ]);
+            exit;
+        } else {
+            //Submit to DB
+            \inspectAndDie('Success');
+        }
+
+
+        \inspectAndDie($errors);
     }
 }
--- public/index.php
+++ public/index.php
@@ -6,10 +6,15 @@ require '../helpers.php';
 
 use Framework\Router;
 
+//Instatiate the router
 $router = new Router();
 
+//Get routes
 $routes = require basePath('routes.php');
+
+//Get current URI and HTTP method
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 
+//Route the request
 $router->route($uri);
